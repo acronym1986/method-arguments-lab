118988f43db3ed22776d10c778e66e2a430fea3b
Tim <acronym1986@gmail.com>
2021-04-27 21:33:24 +1000

    done

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction(name)
+    puts "Hi, my name is #{name}.\n"
+end
+introduction("Josh")
+
+
+
+def introduction_with_language(name1, language)
+    puts "Hi, my name is #{name1} and I am learning to program in #{language}.\n"
+end
+introduction_with_language("Dan", "Ember.js")
